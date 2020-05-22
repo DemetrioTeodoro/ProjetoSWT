f7f5d7a598d41db10f65873c52cf41a582e01ca3
thais <thais@LAPTOP-CS49TKS9>
2020-05-22 15:48:19 -0300

    PainelListagemProfissional

--- src/main/java/testes/menu/PainelListagemProfissionais.java
+++ src/main/java/testes/menu/PainelListagemProfissionais.java
@@ -7,10 +7,20 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JComboBox;
+import javax.swing.JTabbedPane;
 
 public class PainelListagemProfissionais extends JPanel {
 	
 	private TelaCadEditProfissional telaCadEditProfissional = new TelaCadEditProfissional();
+	private JTextField txtNome;
+	private JTextField txtQdeOS;
+	private JTextField txtDataInicio;
+	private JTextField txtDataTermino;
 	
 
 	/**
@@ -18,31 +28,169 @@ public class PainelListagemProfissionais extends JPanel {
 	 */
 	public PainelListagemProfissionais() {
 		
-		JButton btAdicionar = new JButton("Adicionar");
+		JButton btAdicionar = new JButton("Adicionar Profissional");
 		btAdicionar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				telaCadEditProfissional.setVisible(true);
 				telaCadEditProfissional.setLocationRelativeTo(null);
 			}
 		});
+		
+		JLabel lblNewLabel = new JLabel("Profissionais Cadastrados");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		
+		JLabel lblNome = new JLabel("Nome :");
+		
+		txtNome = new JTextField();
+		txtNome.setColumns(10);
+		
+		JLabel lblCategoria = new JLabel("Categoria :");
+		
+		JComboBox cbCategoria = new JComboBox();
+		
+		JLabel lblCidade = new JLabel("Cidade :");
+		
+		JLabel lblQdeOS = new JLabel("Quantidade de Ordem de Servi\u00E7os Finalizadas :");
+		
+		JComboBox cbCidade = new JComboBox();
+		
+		txtQdeOS = new JTextField();
+		txtQdeOS.setColumns(10);
+		
+		JLabel lblPeriodo = new JLabel("Dispon\u00EDvel no Per\u00EDodo :");
+		
+		JLabel lblDataInicio = new JLabel("Data In\u00EDcio");
+		
+		JLabel lblDataTermino = new JLabel("Data T\u00E9rmino");
+		
+		txtDataInicio = new JTextField();
+		txtDataInicio.setColumns(10);
+		
+		txtDataTermino = new JTextField();
+		txtDataTermino.setColumns(10);
+		
+		JButton btnConsultar = new JButton("Consultar");
+		btnConsultar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnDataInicio = new JButton("New button");
+		btnDataInicio.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnDataTermino = new JButton("New button");
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(172)
-					.addComponent(btAdicionar)
-					.addContainerGap(201, Short.MAX_VALUE))
+					.addGap(27)
+					.addComponent(lblNewLabel)
+					.addGap(566)
+					.addComponent(btAdicionar, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(42)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblNome)
+							.addGap(29)
+							.addComponent(txtNome, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblCategoria)
+							.addGap(9)
+							.addComponent(cbCategoria, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblCidade)
+							.addGap(23)
+							.addComponent(cbCidade, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblQdeOS)
+							.addGap(12)
+							.addComponent(txtQdeOS, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE))
+						.addComponent(lblPeriodo)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(45)
+							.addComponent(lblDataInicio, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+							.addGap(79)
+							.addComponent(lblDataTermino, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(45)
+							.addComponent(txtDataInicio, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+							.addGap(1)
+							.addComponent(btnDataInicio, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
+							.addGap(39)
+							.addComponent(txtDataTermino, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+							.addGap(1)
+							.addComponent(btnDataTermino, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(116)
+							.addComponent(btnConsultar)))
+					.addGap(60)
+					.addComponent(tabbedPane, GroupLayout.PREFERRED_SIZE, 523, GroupLayout.PREFERRED_SIZE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addContainerGap(200, Short.MAX_VALUE)
-					.addComponent(btAdicionar)
-					.addGap(77))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(25)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel)
+						.addComponent(btAdicionar))
+					.addGap(26)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(3)
+									.addComponent(lblNome))
+								.addComponent(txtNome, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addGap(18)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(3)
+									.addComponent(lblCategoria))
+								.addComponent(cbCategoria, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addGap(16)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(3)
+									.addComponent(lblCidade))
+								.addComponent(cbCidade, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addGap(22)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(3)
+									.addComponent(lblQdeOS))
+								.addComponent(txtQdeOS, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addGap(18)
+							.addComponent(lblPeriodo)
+							.addGap(18)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblDataInicio)
+								.addComponent(lblDataTermino))
+							.addGap(6)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(1)
+									.addComponent(txtDataInicio, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addComponent(btnDataInicio, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(1)
+									.addComponent(txtDataTermino, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(1)
+									.addComponent(btnDataTermino, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)))
+							.addGap(44)
+							.addComponent(btnConsultar))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(1)
+							.addComponent(tabbedPane, GroupLayout.PREFERRED_SIZE, 521, GroupLayout.PREFERRED_SIZE))))
 		);
 		setLayout(groupLayout);
 		
 
 	}
-	
 }
